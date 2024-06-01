94260a49805cffc9d1b9b86ffaa6b84619c07290
mintball9543 <minseokcomputer@gmail.com>
2024-06-01 11:24:50 +0900

    Week13_2

--- app/src/main/java/com/example/week13_2/MainActivity.java
+++ app/src/main/java/com/example/week13_2/MainActivity.java
@@ -1,21 +1,85 @@
 package com.example.week13_2;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.CallLog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCall;
+    TextView edtCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CALL_LOG}, MODE_PRIVATE);
 
+        btnCall = (Button) findViewById(R.id.btnCall);
+        edtCall = (TextView) findViewById(R.id.edtCall);
 
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                edtCall.setText(getCallHistory());
+            }
+        });
+    }
+
+    public String getCallHistory() {
+        String[] callSet = new String[]{CallLog.Calls.DATE,
+                CallLog.Calls.TYPE, CallLog.Calls.NUMBER,
+                CallLog.Calls.DURATION};
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "not permitted";
+        }
+
+        Cursor c = getContentResolver().query(CallLog.Calls.CONTENT_URI, callSet, null, null, null);
+
+        if (c.getCount() == 0 )  // gabriel
+            return "통화기록 없음";
+
+        StringBuffer callBuff = new StringBuffer();
+        callBuff.append("\n날짜 : 구분 : 전화번호 : 통화시간\n\n");
+        c.moveToFirst();
+        do  {
+            long callDate = c.getLong(0);
+            SimpleDateFormat datePattern = new SimpleDateFormat("yyyy-MM-dd");
+            String date_str = datePattern.format(new Date(callDate));
+            callBuff.append(date_str + ":");
+            if (c.getInt(1) == CallLog.Calls.INCOMING_TYPE)
+                callBuff.append("착신 :");
+            else
+                callBuff.append("발신 :");
+
+            callBuff.append(c.getString(2) + ":");
+            callBuff.append(c.getString(3) + "초\n");
+        } while(c.moveToNext());
+
+        c.close();
+        return callBuff.toString();
     }
 }
